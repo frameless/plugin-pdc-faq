05daa7c386ca39aa36ed0958dc99372a0826ed4f
Mike van den Hoek <mvdhoek1991@gmail.com>
2020-07-30 15:15:30 +0200

    (feat): add depedency checker for pdc-base 2.2.13

--- config/core.php
+++ config/core.php
@@ -2,25 +2,30 @@
 
 return [
     /**
-     * Service Providers.
-     */
+             * Service Providers.
+         */
     'providers' => [
         /**
          * Global providers.
          */
-	    OWC\PDC\FAQ\RestApi\RestApiServiceProvider::class,
-	    OWC\PDC\FAQ\ElasticSearch\ElasticSearchServiceProvider::class,
+        OWC\PDC\FAQ\RestApi\RestApiServiceProvider::class,
+        OWC\PDC\FAQ\ElasticSearch\ElasticSearchServiceProvider::class,
 
-	    /**
+        /**
          * Providers specific to the admin.
          */
         'admin'    => [
-	        OWC\PDC\FAQ\Metabox\MetaboxServiceProvider::class,
+            OWC\PDC\FAQ\Metabox\MetaboxServiceProvider::class,
         ]
     ],
 
     'dependencies' => [
-
+        [
+            'type'    => 'plugin',
+            'label'   => 'OpenPDC Base',
+            'version' => '2.2.13',
+            'file'    => 'pdc-base/pdc-base.php',
+        ],
     ]
 
 ];
--- src/FAQ/RestApi/RestApiServiceProvider.php
+++ src/FAQ/RestApi/RestApiServiceProvider.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Provider which registers the FAQ section to the API.
  */
 
 namespace OWC\PDC\FAQ\RestApi;
 
-use OWC\PDC\Base\Models\Item;
+use OWC\PDC\Base\Repositories\Item;
 use OWC\PDC\Base\Foundation\ServiceProvider;
 
 /**
